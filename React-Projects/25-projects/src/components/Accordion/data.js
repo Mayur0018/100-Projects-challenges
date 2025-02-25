3d4645e166cf32eca629f5a7f2ed58e838744f37
Mayur0018 <mayurnish18@gmail.com>
2025-02-25 20:24:14 +0530

    commit

---
+++ React-Projects/25-projects/src/components/Accordion/data.js
@@ -0,0 +1,29 @@
+ const data = [
+    {
+      "id": 1,
+      "title": "What is React.js?",
+      "content": "React.js is a JavaScript library for building user interfaces, developed by Facebook. It allows developers to create reusable UI components."
+    },
+    {
+      "id": 2,
+      "title": "What is Tailwind CSS?",
+      "content": "Tailwind CSS is a utility-first CSS framework that allows developers to build modern, responsive designs quickly by using pre-defined utility classes."
+    },
+    {
+      "id": 3,
+      "title": "What is Redux Toolkit?",
+      "content": "Redux Toolkit is the official, recommended way to write Redux logic. It simplifies state management and helps developers write scalable Redux applications."
+    },
+    {
+      "id": 4,
+      "title": "How does React Router work?",
+      "content": "React Router enables navigation between different views in a React application by managing the routing and rendering of components dynamically based on the URL."
+    },
+    {
+      "id": 5,
+      "title": "What is Firebase Authentication?",
+      "content": "Firebase Authentication is a backend service by Google that provides authentication solutions using email/password, Google, Facebook, and other providers."
+    }
+  ]
+  
+  export default data;
---
+++ React-Projects/25-projects/src/components/Accordion/index.jsx
@@ -0,0 +1,10 @@
+import data from "./data";
+console.log(data);
+
+export default function Accordian(){
+  return(
+    <>
+    <h1>Prectice Make A Perfect</h1>
+    </>
+  )
+}
