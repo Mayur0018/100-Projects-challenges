364fed62eaac8776760258897fdd42be04cfaa7f
Mayur0018 <mayurnish18@gmail.com>
2025-04-11 22:44:29 +0530

    commit

---
+++ Node Projects/hello-prisma/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+// Create User
+app.post('/user', async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: { name, email },
+    });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Get All Users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
