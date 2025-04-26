1ab405450024d3326c1b69b3066a080f41d3b440
Mayur0018 <mayurnish18@gmail.com>
2025-04-26 10:29:03 +0530

    comiiit

---
+++ Js Projects/Sheriyansh JS/script.js
@@ -0,0 +1,79 @@
+// console.log("Mayur");
+
+// let currentyear = new Date();
+// console.log(currentyear.getFullYear());
+
+// // concat the name
+// let NameA = "Mayur";
+// let NameB = "Nishad";
+// console.log(NameA.concat(NameB));
+
+// console.error("Enter A Valid Code");
+
+// var a = true;
+// console.log(typeof a);
+
+// let age = prompt("Enter The Age");
+
+// if (age > 18) {
+//   console.log("You Are Eligible To Give A Vote");
+// } else {
+//   console.log("You Are Not Eligible To Give A Vote");
+// }
+
+// variabls qustions
+// let a = 12;
+// console.log(a);
+
+// const pivalues = Math.PI;
+// console.log(pivalues);
+
+// let c = 20;
+// c = 40;
+
+// console.log(c);
+
+// var d = "25";
+// console.log(typeof(d));
+
+// let a = 45;
+// console.log(a);
+
+// let b = "gabu samijame";
+// console.log(b);
+
+// let c = true;
+// console.log(c);
+
+// const arr =  [10,20,30,40,50];
+// console.log(arr);
+
+// const me updation allowed hai lekin reasign not allowed
+
+// for loop qustion
+
+// for(let i = 0; i<=50; i++){
+//     console.log(i);
+// }
+
+// sum of 1 to 10 numbers
+
+// for (let i = 1; i <= 10; i++) {
+//   let sum = i + i
+//   console.log(sum);
+// }
+
+// let a = 1;
+// let sum = 0;
+// while (a <= 10) {
+//    sum = a + sum 
+//   a++;
+// }
+
+// console.log(sum);
+
+// let a ="Javascript";
+
+// for( let Strvalues of a){
+//     console.log(Strvalues);
+// }
