4534ad52da9ede1c533e50617ffff19b674bbff0
Mayur0018 <mayurnish18@gmail.com>
2025-07-16 22:56:06 +0530

    CO,MMMOY

--- Js Projects/Sheriyansh JS/script.js
+++ Js Projects/Sheriyansh JS/script.js
@@ -566,3 +566,17 @@
 //   abcd.style.top = deft.clientY + "px";
 //   abcd.style.left = deft.clientX + "px";
 // });
+let input = document.querySelector("input");
+let span = document.querySelector("span");
+
+input.addEventListener("input", function () {
+
+  let left = 20 - input.value.length;
+  span.textContent = left;
+ 
+  if (left < 0) {
+    span.style.color = "red";
+  } else {
+     span.style.color = "black"
+  }
+});
