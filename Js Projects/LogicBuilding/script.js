65dbc5fdf11e4626b1d9bf2676317172bc672f48
Mayur0018 <mayurnish18@gmail.com>
2025-11-27 13:24:12 +0530

    creating a snake game

--- Js Projects/LogicBuilding/script.js
+++ Js Projects/LogicBuilding/script.js
@@ -1,15 +1,53 @@
 let board = document.querySelector(".board");
-let blockheight = 30;
-let blockwidth = 30;
+let blockheight = 50;
+let blockwidth = 50;
 
 const cols = Math.floor(board.clientHeight / blockheight);
 const rows = Math.floor(board.clientWidth / blockwidth);
 board.addEventListener("click", () => {
   console.log("mayur");
 });
+const blocks = [];
 
-for (let i = 0; i < rows * cols; i++) {
-  const block = document.createElement("div");
-  block.classList.add("block");
-  board.appendChild(block);
+const snake = [
+  {
+    x: 1,
+    y: 3,
+  },
+];
+let direaction = "up";
+for (let row = 0; row < rows; row++) {
+  for (let col = 0; col < cols; col++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    board.appendChild(block);
+    blocks[`${row}-${col}`] = block;
+  }
 }
+
+function render() {
+  snake.forEach((segment) => {
+    console.log(segment);
+    blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+  });
+}
+
+setInterval(() => {
+  let head = null;
+  if (direaction === "left") {
+    head = { x: snake[0].x, y: snake[0].y - 1 };
+  } else if (direaction === "right") {
+    head = { x: snake[0].x, y: snake[0].y + 1 };
+  } else if (direaction === "down") {
+    head = { x: snake[0].x + 1, y: snake[0].y };
+  } else if (direaction === "up") {
+    head = { x: snake[0].x - 1, y: snake[0].y };
+  }
+
+  snake.forEach((segment) => {
+    blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+  });
+  snake.unshift(head);
+  snake.pop();
+  render();
+}, 400);
