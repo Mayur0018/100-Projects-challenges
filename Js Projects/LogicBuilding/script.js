3903740f9ba1fe43493814a9a89c853c24f89436
Mayur0018 <mayurnish18@gmail.com>
2025-12-03 11:50:18 +0530

    Logic Building in Js

--- Js Projects/LogicBuilding/script.js
+++ Js Projects/LogicBuilding/script.js
@@ -32,22 +32,38 @@ function render() {
   });
 }
 
-setInterval(() => {
-  let head = null;
-  if (direaction === "left") {
-    head = { x: snake[0].x, y: snake[0].y - 1 };
-  } else if (direaction === "right") {
-    head = { x: snake[0].x, y: snake[0].y + 1 };
-  } else if (direaction === "down") {
-    head = { x: snake[0].x + 1, y: snake[0].y };
-  } else if (direaction === "up") {
-    head = { x: snake[0].x - 1, y: snake[0].y };
-  }
+// setInterval(() => {
+//   let head = null;
+//   if (direaction === "left") {
+//     head = { x: snake[0].x, y: snake[0].y - 1 };
+//   } else if (direaction === "right") {
+//     head = { x: snake[0].x, y: snake[0].y + 1 };
+//   } else if (direaction === "down") {
+//     head = { x: snake[0].x + 1, y: snake[0].y };
+//   } else if (direaction === "up") {
+//     head = { x: snake[0].x - 1, y: snake[0].y };
+//   }
 
-  snake.forEach((segment) => {
-    blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
-  });
-  snake.unshift(head);
-  snake.pop();
-  render();
-}, 400);
+//   snake.forEach((segment) => {
+//     blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+//   });
+//   snake.unshift(head);
+//   snake.pop();
+//   render();
+// }, 400);
+
+// ArrowUp
+// script.js:56 ArrowRight
+// script.js:56 ArrowDown
+// script.js:56 ArrowLeft
+addEventListener("keydown", (event) => {
+  if (event.key == "ArrowUp") {
+    direaction = "up";
+  } else if (event.key == "ArrowRight") {
+    direaction = "right";
+  } else if (event.key == "ArrowDown") {
+    direaction = "down";
+  } else if ((event.key = "ArrowLeft")) {
+    direaction = "left";
+  }
+});
